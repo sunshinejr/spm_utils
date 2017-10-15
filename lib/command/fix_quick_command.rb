08d5950cb027665b09a8bd239ea0a73cf0339034
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2017-10-15 20:03:57 +0200

    Make the first build of quick command working

--- lib/command/fix_quick_command.rb
+++ lib/command/fix_quick_command.rb
@@ -1,8 +1,4 @@
-require 'clamp'
-require 'project_mod'
-
 class FixQuickCommand < Clamp::Command
-
     def execute
         ProjectMod.apply_build_setting(name: 'CLANG_ENABLE_MODULES', value: 'YES', target_name: 'QuickSpecBase')
         puts "Quick fixed!"        
--- lib/project_mod.rb
+++ lib/project_mod.rb
@@ -1,31 +1,29 @@
 require 'xcodeproj'
 
-module SPMFixers
-    class ProjectMod
-        def self.apply_build_setting(name:, value:, target_name: nil, depedencies_required: [])
-            xcodeprojs = Dir.new(Dir.pwd).select { |a| a.include? '.xcodeproj' }
-            xcodeprojs_count = xcodeprojs.count
-            
-            abort "No *.xcodeproj files in current directory" if xcodeprojs_count == 0
-            abort "Found #{xcodeprojs_count} .xcodeproj files in the directory (1 required)." if xcodeprojs_count > 1
-            
-            project = Xcodeproj::Project.open(xcodeprojs[0])
-            targets = project.targets.clone
-            if target_name != nil
-                targets.select! { |t| t.name == target_name }
-            end
-            if !depedencies_required.empty?
-                targets.select! { |a| depedencies_required.include? a.name }
-            end
-            
-            abort "No targets" if targets.empty?
-            
-            target = targets[0]
-            target.build_configurations.each do |config|
-                config.build_settings[name] = value
-            end
-            
-            project.save()
+class ProjectMod
+    def self.apply_build_setting(name:, value:, target_name: nil, depedencies_required: [])
+        xcodeprojs = Dir.new(Dir.pwd).select { |a| a.include? '.xcodeproj' }
+        xcodeprojs_count = xcodeprojs.count
+        
+        abort "No *.xcodeproj files in current directory" if xcodeprojs_count == 0
+        abort "Found #{xcodeprojs_count} .xcodeproj files in the directory (1 required)." if xcodeprojs_count > 1
+        
+        project = Xcodeproj::Project.open(xcodeprojs[0])
+        targets = project.targets.clone
+        if target_name != nil
+            targets.select! { |t| t.name == target_name }
         end
+        if !depedencies_required.empty?
+            targets.select! { |a| depedencies_required.include? a.name }
+        end
+        
+        abort "No targets" if targets.empty?
+        
+        target = targets[0]
+        target.build_configurations.each do |config|
+            config.build_settings[name] = value
+        end
+        
+        project.save()
     end
 end
