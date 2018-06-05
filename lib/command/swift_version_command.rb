7d246b7764ede638ab42c5d5d1ae76372b83327e
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2018-06-05 22:18:44 +0200

    Add Swift 4.2 option to swift subcommand

--- lib/command/swift_version_command.rb
+++ lib/command/swift_version_command.rb
@@ -4,7 +4,7 @@ class SwiftVersionCommand < Clamp::Command
     # option ['--swift-version'], 'SWIFT_VERSION', 'What Swift version to change into', :required => true
 
     def execute
-        supported_swift_versions = ['3', '4']
+        supported_swift_versions = ['3.0', '4.0', '4.2']
         if !supported_swift_versions.include? swift_version
             signal_usage_error "'#{swift_version}' swift version is not supported. Supported values: " + supported_swift_versions.map { |v| "'#{v}'" }.join(", ")
         end
